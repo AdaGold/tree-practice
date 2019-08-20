f9fab975376c35b3ca34e2db9f27c09de4ab4181
CheezItMan <chris@adadevelopersacademy.org>
2019-08-19 21:26:17 -0700

    update for students

--- lib/tree.rb
+++ lib/tree.rb
@@ -6,71 +6,6 @@ class TreeNode
      @left = nil
      @right = nil
    end
-
-   def add(val)
-      if val <= @value
-        if left.nil?
-          @left = TreeNode.new(val)
-        else
-          @left.add(val)
-        end
-      else
-        if right.nil?
-          @right = TreeNode.new(val)
-        else
-          @right.add(val)
-        end
-      end
-   end
-
-   def find(val)
-    if val == @value
-      return true
-    elsif val <= @value
-      return false if @left.nil?
-      return @left.find(val)
-    else
-      return false if @right.nil?
-      return @right.find(val)
-    end
-   end
-
-   def inorder(array)
-      left.inorder(array) unless left.nil? 
-      array << @value
-      right.inorder(array) unless right.nil?
-   end
-
-   def preorder(array)
-     array << @value
-     left.preorder(array) unless left.nil?
-     right.preorder(array) unless right.nil?
-   end
-
-   def postorder(array)
-    left.postorder(array) unless left.nil?
-    right.postorder(array) unless right.nil?
-    array << @value
-  end
-
-  def bfs(list)
-    queue = [self]
-
-    return bfs_helper(queue, list)
-  end
-
-  private 
-
-    def bfs_helper(queue, list)
-      return list if queue.size == 0
-      first_item = queue.shift
-      list << first_item.value
-      queue << first_item.left unless first_item.left.nil?
-      queue << first_item.right unless first_item.right.nil?
-
-      return bfs_helper(queue, list)
-
-    end
 end
 
 class Tree
@@ -79,41 +14,40 @@ class Tree
     @root = nil
   end
 
+  # Time Complexity: 
+  # Space Complexity: 
   def add(value)
-    if @root == nil
-      @root = TreeNode.new(value)
-    else
-      @root.add(value)
-    end
+    raise NotImplementedError
   end
 
+  # Time Complexity: 
+  # Space Complexity: 
   def find(val)
-    return false if @root.nil?
-    return @root.find(val)
+    raise NotImplementedError
   end
 
+  # Time Complexity: 
+  # Space Complexity: 
   def inorder
-    list = []
-    @root.inorder(list) unless @root.nil?
-    return list
+    raise NotImplementedError
   end
 
+  # Time Complexity: 
+  # Space Complexity: 
   def preorder
-    list = []
-    @root.preorder(list) unless @root.nil?
-    return list
+    raise NotImplementedError
   end
 
+  # Time Complexity: 
+  # Space Complexity: 
   def postorder
-    list = []
-    @root.postorder(list) unless @root.nil?
-    return list
+    raise NotImplementedError
   end
 
+  # Time Complexity: 
+  # Space Complexity: 
   def bfs
-    list = []
-    return @root.bfs(list) unless @root.nil?
-    return list
+    raise NotImplementedError
   end
 
   # Useful for printing
