3a94a0f23aa983a49a5a6ecf271bbbab3fe3d534
CheezItMan <chris@adadevelopersacademy.org>
2019-08-20 20:45:57 -0700

    updated tests to key-value pairs

--- test/tree_test.rb
+++ test/tree_test.rb
@@ -1,5 +1,5 @@
 require_relative 'test_helper'
-require "minitest/skip_dsl"
+
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
@@ -7,29 +7,30 @@ describe Tree do
   let (:tree) {Tree.new}
 
   def add_nodes(tree)
-    tree.add(5)
-    tree.add(3)
-    tree.add(7)
-    tree.add(27)
-    tree.add(4)
+    tree.add(5, "Peter")
+    tree.add(3, "Paul")
+    tree.add(1, "Mary")
+    tree.add(10, "Karla")
+    tree.add(15, "Ada")
+    tree.add(25, "Kari")
   end
 
   it "add & find values" do
-    tree.add(5)
-    expect(tree.find(5)).must_equal true
+    tree.add(5, "Peter")
+    expect(tree.find(5)).must_equal "Peter"
 
-    tree.add(15)
-    expect(tree.find(15)).must_equal true
+    tree.add(15, "Ada")
+    expect(tree.find(15)).must_equal "Ada"
 
-    tree.add(3)
-    expect(tree.find(3)).must_equal true
+    tree.add(3, "Paul")
+    expect(tree.find(3)).must_equal "Paul"
   end
 
   it "can't find anything when the tree is empty" do
-    expect(tree.find(50)).must_equal false
+    expect(tree.find(50)).must_be_nil
   end
 
-  xdescribe "inorder" do
+  describe "inorder" do
     it "will give an empty array for an empty tree" do
       expect(tree.inorder).must_equal []
     end
@@ -37,12 +38,14 @@ describe Tree do
     it "will return the tree in order" do
       add_nodes(tree)
 
-      expect(tree.inorder).must_equal [3, 4, 5, 7, 27]
+      expect(tree.inorder).must_equal [{:key=>1, :value=>"Mary"}, {:key=>3, :value=>"Paul"}, 
+                                       {:key=>5, :value=>"Peter"}, {:key=>10, :value=>"Karla"}, 
+                                       {:key=>15, :value=>"Ada"}, {:key=>25, :value=>"Kari"}]
     end
   end
 
 
-  xdescribe "preorder" do
+  describe "preorder" do
     it "will give an empty array for an empty tree" do
       expect(tree.preorder).must_equal []
     end
@@ -50,11 +53,13 @@ describe Tree do
     it "will return the tree in preorder" do
       add_nodes(tree)
 
-      expect(tree.preorder).must_equal [5, 3, 4, 7, 27]
+      expect(tree.preorder).must_equal [{:key=>5, :value=>"Peter"}, {:key=>3, :value=>"Paul"}, 
+                                        {:key=>1, :value=>"Mary"}, {:key=>10, :value=>"Karla"}, 
+                                        {:key=>15, :value=>"Ada"}, {:key=>25, :value=>"Kari"}]
     end
   end
 
-  xdescribe "postorder" do
+  describe "postorder" do
     it "will give an empty array for an empty tree" do
       expect(tree.postorder).must_equal []
     end
@@ -62,45 +67,22 @@ describe Tree do
     it "will return the tree in postorder" do
       add_nodes(tree)
 
-      expect(tree.postorder).must_equal [4, 3, 27, 7, 5]
+      expect(tree.postorder).must_equal [{:key=>1, :value=>"Mary"}, {:key=>3, :value=>"Paul"}, 
+                                         {:key=>25, :value=>"Kari"}, {:key=>15, :value=>"Ada"}, 
+                                         {:key=>10, :value=>"Karla"}, {:key=>5, :value=>"Peter"}]
     end
   end
 
-    # This is an OPTIONAL method
-  xdescribe "breadth first search" do
+  describe "breadth first search" do
     it "will give an empty array for an empty tree" do
       expect(tree.bfs).must_equal []
     end
 
     it "will return an array of a level-by-level output of the tree" do
       add_nodes(tree)
-      expect(tree.bfs).must_equal [5, 3, 7, 4, 27]
-    end
-  end
-
-  xdescribe "height" do
-    it "will return 0 for an empty tree" do
-      expect(tree.height).must_equal 0
-    end
-
-    it "will return the height for a given tree" do
-      tree.add(5)
-      expect(tree.height).must_equal 1
-
-      tree.add(10)
-      expect(tree.height).must_equal 2
-
-      tree.add(20)
-      expect(tree.height).must_equal 3
-
-      tree.add(2)
-      expect(tree.height).must_equal 3
-
-      tree.add(3)
-      expect(tree.height).must_equal 3
-
-      tree.add(4)
-      expect(tree.height).must_equal 4
+      expect(tree.bfs).must_equal [{:key=>5, :value=>"Peter"}, {:key=>3, :value=>"Paul"}, 
+                                   {:key=>10, :value=>"Karla"}, {:key=>1, :value=>"Mary"}, 
+                                   {:key=>15, :value=>"Ada"}, {:key=>25, :value=>"Kari"}]
     end
   end
 end
