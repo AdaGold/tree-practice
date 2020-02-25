3022b632721f1b5fdbb0738ecd116495d9457d8f
CheezItMan <chris@adadevelopersacademy.org>
2020-02-24 19:45:41 -0800

    added height tests

--- test/tree_test.rb
+++ test/tree_test.rb
@@ -1,4 +1,7 @@
-require_relative "test_helper"
+require_relative 'test_helper'
+
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe Tree do
   let (:tree) {Tree.new}
@@ -13,21 +16,19 @@ describe Tree do
     tree
   }
 
-  describe "add and find" do 
-    it "add & find values" do
-      tree.add(5, "Peter")
-      expect(tree.find(5)).must_equal "Peter"
+  it "add & find values" do
+    tree.add(5, "Peter")
+    expect(tree.find(5)).must_equal "Peter"
 
-      tree.add(15, "Ada")
-      expect(tree.find(15)).must_equal "Ada"
+    tree.add(15, "Ada")
+    expect(tree.find(15)).must_equal "Ada"
 
-      tree.add(3, "Paul")
-      expect(tree.find(3)).must_equal "Paul"
-    end
+    tree.add(3, "Paul")
+    expect(tree.find(3)).must_equal "Paul"
+  end
 
-    it "can't find anything when the tree is empty" do
-      expect(tree.find(50)).must_be_nil
-    end
+  it "can't find anything when the tree is empty" do
+    expect(tree.find(50)).must_be_nil
   end
 
   describe "inorder" do
@@ -36,6 +37,7 @@ describe Tree do
     end
 
     it "will return the tree in order" do
+
       expect(tree_with_nodes.inorder).must_equal [{:key=>1, :value=>"Mary"}, {:key=>3, :value=>"Paul"}, 
                                        {:key=>5, :value=>"Peter"}, {:key=>10, :value=>"Karla"}, 
                                        {:key=>15, :value=>"Ada"}, {:key=>25, :value=>"Kari"}]
@@ -78,45 +80,47 @@ describe Tree do
                                    {:key=>15, :value=>"Ada"}, {:key=>25, :value=>"Kari"}]
     end
   end
-  
-  describe "height" do 
-    it "will return 0 if tree is empty" do 
-      expect(tree.height()).must_equal 0
+
+  describe "height" do
+    it "will return 0 for an empty tree" do
+      my_tree = Tree.new
+
+      expect(tree.height).must_equal 0
     end
 
-    it "will return the nuber of nodes in the longest path" do
-      expect(tree_with_nodes.height).must_equal 4
-      tree_with_nodes.add(60, "sam")
-      tree_with_nodes.add(58, "penny")
-      tree_with_nodes.add(65, "sam")
-      expect(tree_with_nodes.height).must_equal 6
-    end
-    
-    it "will give the correct height of a binary search tree" do
-      tree_with_nodes.add(30, "Tatiana")
-      expect(tree_with_nodes.height).must_equal 5
-    end
-  end
+    it "will return 1 for a tree of height 1" do
+      my_tree = Tree.new
 
-  describe "delete" do
-    it "can delete a note in the tree" do
-      # Arrange & Assert
-      expect(tree_with_nodes.find(15)).must_equal "Ada"
-
-      # Act
-      tree_with_nodes.delete(15)
-
-      # Assert
-      expect(tree_with_nodes.find(15)).must_be_nil
+      my_tree.add(100)
+      expect(my_tree.height).must_equal 1
     end
 
-    it "will return nil if the node is not in the tree when it's deleted" do
-      # Arrange & Act
-      answer = tree_with_nodes.delete(47)
+    it "will report the height for a balanced tree" do
+      expect(tree_with_nodes.height).must_equal 3
+    end
 
-      # Assert
-      expect(answer).must_be_nil
-      expect(tree_with_nodes.find(47)).must_be_nil
+    it "will report the height for unbalanced trees" do
+      my_tree = Tree.new
+
+      my_tree.add(100)
+      my_tree.add(110)
+      my_tree.add(120)
+      my_tree.add(130)
+      my_tree.add(140)
+
+      expect(my_tree.height).must_equal 5
+
+      my_tree = Tree.new
+
+      my_tree = Tree.new
+
+      my_tree.add(100)
+      my_tree.add(90)
+      my_tree.add(80)
+      my_tree.add(70)
+      my_tree.add(60)
+
+      expect(my_tree.height).must_equal 5
     end
   end
 end
