7001c989ed2da9509038b714d2ebf1dc4ef4db7d
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-02 19:42:26 -0700

    bst starter code - python

---
+++ binary_search_tree/tree.py
@@ -0,0 +1,51 @@
+class TreeNode:
+    def __init__(self, key, val = None):
+        if val == None:
+            val = key
+
+        self.key = key
+        self.value = val
+        self.left = None
+        self.right = None
+        
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    # Time Complexity: 
+    # Space Complexity: 
+    def add(self, key, value = None):
+        pass
+
+    # Time Complexity: 
+    # Space Complexity: 
+    def find(self, key):
+        pass
+
+    def inorder(self):
+        pass
+
+    def preorder(self):
+        pass
+
+    def postorder(self):
+        pass
+
+    def height(self):
+        pass
+
+
+#   # Optional Method
+#   # Time Complexity: 
+#   # Space Complexity: 
+    def bfs(self):
+        pass
+
+        
+
+
+#   # Useful for printing
+    def to_s(self):
+        return f"{self.inorder()}"
