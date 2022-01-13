d22318fb44ef75f6fde61a6f714e66cabbace1f3
Chris McAnally <chris@adadevelopersacademy.org>
2022-01-13 11:43:57 -0800

    update tests for C16

--- tests/test_binary_search_tree.py
+++ tests/test_binary_search_tree.py
@@ -6,6 +6,7 @@ from binary_search_tree.tree import Tree
 def empty_tree() -> Tree():
     return Tree()
 
+
 @pytest.fixture()
 def tree_with_nodes(empty_tree) -> Tree():
     empty_tree.add(5, "Peter")
@@ -21,14 +22,17 @@ def tree_with_nodes(empty_tree) -> Tree():
 def test_find_returns_none_for_empty_tree(empty_tree):
     assert empty_tree.find(5) == None
 
+
 def test_can_find_single_root_node(empty_tree):
     empty_tree.add(25, "Kari")
     assert empty_tree.find(25) == "Kari"
+
 
 def test_add_and_find(tree_with_nodes):
     assert tree_with_nodes.find(5) == "Peter"
     assert tree_with_nodes.find(15) == "Ada"
     assert tree_with_nodes.find(3) == "Paul"
+
 
 def test_can_find_leaf_nodes(tree_with_nodes):
     assert tree_with_nodes.find(1) == "Mary"
@@ -40,34 +44,36 @@ def test_can_find_leaf_nodes(tree_with_nodes):
 def test_find_returns_none_for_values_not_in_tree(tree_with_nodes):
     assert tree_with_nodes.find(6) == None
 
+
 def test_inorder_with_empty_tree(empty_tree):
     answer = empty_tree.inorder()
     assert empty_tree.inorder() == []
 
+
 def test_inorder_with_nodes(tree_with_nodes):
     expected_answer = [
         {
-            "key": 1, 
+            "key": 1,
             "value": "Mary"
-        }, 
+        },
         {
-            "key": 3, 
+            "key": 3,
             "value": "Paul"
-        }, 
+        },
         {
-            "key": 5, 
+            "key": 5,
             "value": "Peter"
-        }, 
+        },
         {
-            "key": 10, 
+            "key": 10,
             "value": "Karla"
-        }, 
+        },
         {
-            "key": 15, 
+            "key": 15,
             "value": "Ada"
-        }, 
+        },
         {
-            "key": 25, 
+            "key": 25,
             "value": "Kari"
         }
     ]
@@ -75,31 +81,33 @@ def test_inorder_with_nodes(tree_with_nodes):
     answer = tree_with_nodes.inorder()
     assert answer == expected_answer
 
+
 def test_preorder_on_empty_tree(empty_tree):
     assert empty_tree.preorder() == []
+
 
 def test_preorder_on_tree_with_nodes(tree_with_nodes):
     expected_answer = [
         {
-            "key": 5, 
+            "key": 5,
             "value": "Peter"
-        }, 
+        },
         {
-            "key": 3, 
+            "key": 3,
             "value": "Paul"
         },
         {
             "key": 1,
             "value": "Mary"
-        }, 
+        },
         {
             "key": 10,
             "value": "Karla"
-        }, 
+        },
         {
             "key": 15,
             "value": "Ada"
-        }, 
+        },
         {
             "key": 25,
             "value": "Kari"
@@ -109,27 +117,29 @@ def test_preorder_on_tree_with_nodes(tree_with_nodes):
     answer = tree_with_nodes.preorder()
     assert answer == expected_answer
 
+
 def test_postorder_on_empty_tree(empty_tree):
     assert empty_tree.postorder() == []
+
 
 def test_postorder_on_tree_with_nodes(tree_with_nodes):
     expected_answer = [
         {
-            "key": 1, 
+            "key": 1,
             "value": "Mary"
-        }, 
+        },
         {
             "key": 3,
             "value": "Paul"
         },
         {
-            "key": 25, 
+            "key": 25,
             "value": "Kari"
-        }, 
+        },
         {
             "key": 15,
             "value": "Ada"
-        }, 
+        },
         {
             "key": 10,
             "value": "Karla"
@@ -143,44 +153,82 @@ def test_postorder_on_tree_with_nodes(tree_with_nodes):
     answer = tree_with_nodes.postorder()
     assert answer == expected_answer
 
+
 def test_height_of_empty_tree_is_zero(empty_tree):
     assert empty_tree.height() == 0
+
 
 def test_height_of_one_node_tree(empty_tree):
     empty_tree.add(5, "pasta")
     assert empty_tree.height() == 1
+
 
 def test_height_of_many_node_tree(tree_with_nodes):
     assert tree_with_nodes.height() == 4
     tree_with_nodes.add(2, "pasta")
     tree_with_nodes.add(2.5, "bread")
     assert tree_with_nodes.height() == 5
-    
+
+
+def test_will_report_height_of_balanced_tree():
+    balanced_tree = Tree()
+    balanced_tree.add(5, "Peter")
+    balanced_tree.add(3, "Paul")
+    balanced_tree.add(4, "Kate")
+    balanced_tree.add(1, "Mary")
+    balanced_tree.add(10, "Karla")
+    balanced_tree.add(8, "Ada")
+    balanced_tree.add(25, "Kari")
+
+    assert balanced_tree.height() == 3
+
+
+def test_will_report_height_of_unbalanced_tree():
+    unbalanced_tree = Tree()
+    unbalanced_tree.add(100, "Peter")
+    unbalanced_tree.add(110, "Paul")
+    unbalanced_tree.add(120, "Kate")
+    unbalanced_tree.add(130, "Mary")
+    unbalanced_tree.add(140, "Karla")
+
+    assert unbalanced_tree.height() == 5
+
+    unbalanced_tree = Tree()
+    unbalanced_tree.add(140, "Peter")
+    unbalanced_tree.add(130, "Paul")
+    unbalanced_tree.add(120, "Kate")
+    unbalanced_tree.add(110, "Mary")
+    unbalanced_tree.add(100, "Karla")
+
+    assert unbalanced_tree.height() == 5
+
+
 def test_bfs_with_empty_tree(empty_tree):
     assert empty_tree.bfs() == []
+
 
 def test_bfs_with_tree_with_nodes(tree_with_nodes):
     expected_answer = [
         {
-            "key": 5, 
+            "key": 5,
             "value": "Peter"
-        }, 
+        },
         {
-            "key": 3, 
+            "key": 3,
             "value": "Paul"
-        }, 
+        },
         {
-            "key": 10, 
+            "key": 10,
             "value": "Karla"
-        }, 
+        },
         {
-            "key": 1, 
+            "key": 1,
             "value": "Mary"
-        }, 
+        },
         {
             "key": 15,
             "value": "Ada"
-        }, 
+        },
         {
             "key": 25,
             "value": "Kari"
